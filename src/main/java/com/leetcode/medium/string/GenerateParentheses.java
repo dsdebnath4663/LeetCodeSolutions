9ab9f655e11028f6ffc6e4c5ef26a27d4b2f1398
debnath <dsdebnath4663@gmail.com>
2022-04-12 19:40:14 +0530

    22. Generate Parentheses https://leetcode.com/problems/generate-parentheses/

---
+++ src/main/java/com/leetcode/medium/string/GenerateParentheses.java
@@ -0,0 +1,29 @@
+package com.leetcode.medium.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public static List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public static void backtrack(List<String> list, String str, int open, int close, int max) {
+
+        if (str.length() == max * 2) {
+            list.add(str);
+            return;
+        }
+
+        if (open < max)
+            backtrack(list, str + "(", open + 1, close, max);
+        if (close < open)
+            backtrack(list, str + ")", open, close + 1, max);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+    }
+}
