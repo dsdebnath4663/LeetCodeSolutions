3281c03c405c75dad622fc0143fc03b6f1911705
debnath <dsdebnath4663@gmail.com>
2022-04-12 23:09:34 +0530

    22. Generate Parentheses https://leetcode.com/problems/generate-parentheses/ 49. Group Anagrams https://leetcode.com/problems/group-anagrams/

---
+++ src/main/java/com/leetcode/medium/string/GroupAnagrams.java
@@ -0,0 +1,27 @@
+package com.leetcode.medium.string;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
