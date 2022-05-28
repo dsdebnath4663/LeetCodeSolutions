5096613659f7a88ef7b1483ccc8f4da0447c5152
debnath <dsdebnath4663@gmail.com>
2022-05-28 22:59:36 +0530

    331. Verify Preorder Serialization of a Binary Tree

---
+++ src/main/java/com/leetcode/medium/string/VerifyPreorderSerializationofaBinaryTree.java
@@ -0,0 +1,17 @@
+package com.leetcode.medium.string;
+
+public class VerifyPreorderSerializationofaBinaryTree {
+    public static boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int diff = 1;
+        for (String node: nodes){
+            if(--diff < 0 ) return false;
+            if (!node.equals ("#")) diff += 2;
+        }
+        return diff == 0;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+    }
+}
