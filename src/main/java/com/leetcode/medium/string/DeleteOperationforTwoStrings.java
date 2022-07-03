4f492634ffd2a6399b98ae1933d0457fab2b9900
debnath <dsdebnath4663@gmail.com>
2022-07-03 20:39:34 +0530

    583. Delete Operation for Two Strings https://leetcode.com/problems/delete-operation-for-two-strings/

---
+++ src/main/java/com/leetcode/medium/string/DeleteOperationforTwoStrings.java
@@ -0,0 +1,24 @@
+package com.leetcode.medium.string;
+
+import sun.text.normalizer.UCharacter;
+
+public class DeleteOperationforTwoStrings {
+    public static int minDistance(String s1, String s2){
+        int[][] memo = new int[s1.length()+1][s2.length()+1];
+        return s1.length() + s2.length()-2* lcs (s1,s2,s1.length(),s2.length(),memo);
+    }
+    public static int lcs(String s1, String s2, int m, int n, int[][] memo){
+        if (m == 0 || n == 0)
+            return 0;
+        if (memo[m][n]>0)
+            return memo [m][n];
+        if (s1.charAt(m - 1) == s2.charAt(n-1))
+            memo [m][n] = 1 + lcs(s1, s2, m-1, n-1, memo);
+        else
+            memo[m][n]= Math.max(lcs(s1,s2,m,n-1, memo),lcs(s1,s2,m-1,n,memo));
+        return memo[m][n];
+    }
+    public static void main(String[] args) {
+    System.out.println(minDistance("sea","eat"));
+    }
+}
---
+++ src/main/java/com/leetcode/medium/string/MapSumPairs.java
@@ -0,0 +1,43 @@
+package com.leetcode.medium.string;
+
+import java.util.*;
+
+public class MapSumPairs {
+    static Map<String, Integer> map ;
+    static Map<String, Integer> score;
+
+    public MapSumPairs() {
+        map = new HashMap();
+        score = new HashMap();
+    }
+
+    public static void insert(String key, int val) {
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        String prefix = "";
+        for (char c : key.toCharArray()) {
+            prefix += c;
+            score.put(prefix, score.getOrDefault(prefix, 0) + delta);
+        }
+
+
+        for(Map.Entry<String,Integer> mapData:map.entrySet()){
+                System.out.println("Key : "+mapData.getKey()+ " Value : "+mapData.getValue());
+        }
+    }
+
+    public static int sum(String prefix) {
+        int val = score.getOrDefault(prefix, 0);
+        System.out.println("Current sum is : " + val);
+        return val;
+    }
+
+    public static void main(String args[]) {
+        MapSumPairs mapSum = new MapSumPairs();
+        mapSum.insert("apple", 3);
+        mapSum.sum("ap");           // return 3 (apple = 3)
+        mapSum.insert("app", 2);
+        mapSum.sum("ap");           // return 5 (apple + app = 3 + 2 = 5)
+    }
+
+}
