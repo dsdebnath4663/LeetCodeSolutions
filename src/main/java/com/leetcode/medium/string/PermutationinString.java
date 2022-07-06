8f6adff8ed8137b25e87146914ec7892dcb530e0
debnath <dsdebnath4663@gmail.com>
2022-07-06 15:00:12 +0530

    567. Permutation in String https://leetcode.com/problems/permutation-in-string/

--- src/main/java/com/leetcode/medium/string/PermutationinString.java
+++ src/main/java/com/leetcode/medium/string/PermutationinString.java
@@ -1,33 +1,31 @@
 package com.leetcode.medium.string;
 
 public class PermutationinString {
-    boolean flag = false ;
-
-    public boolean checkInclusion(String s1, String s2) {
-        permute(s1, s2, 0);
-        return flag;
-    }
-
-    public String swap(String s, int i0, int i1) {
-        if (i0 == i1)
-            return s;
-        String s1 = s.substring(0, i0);
-        String s2 = s.substring(i0 + 1, i1);
-        String s3 = s.substring(i1 + 1);
-        return s1 + s.charAt(i1) + s2 + s.charAt(i0) + s3;
-    }
-
-    void permute(String s1, String s2, int l) {
-        if (l == s1.length()) {
-            if (s2.indexOf(s1) >= 0)
-                flag = true;
-        } else {
-            for (int i = l; i < s1.length(); i++) {
-                s1 = swap(s1, l, i);
-                permute(s1, s2, l + 1);
-                s1 = swap(s1, l, i);
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1map[s1.charAt(i) - 'a']++;
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2map = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2map[s2.charAt(i + j) - 'a']++;
             }
+            if (matches(s1map, s2map))
+                return true;
         }
+        return false;
     }
 
+    public static boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(checkInclusion("ab", "eidbaooo"));
+    }
 }
