59c836ef52fa8e50dbef78a31519cf76c812bc3e
debnath <dsdebnath4663@gmail.com>
2022-08-18 10:59:33 +0530

    1.Two Sum https://leetcode.com/problems/two-sum/

---
+++ src/main/java/com/leetcode/easy/string/TwoSum.java
@@ -0,0 +1,27 @@
+package com.leetcode.easy.string;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;
+    }
+    public static void main(String[] args ){
+        int[] numbers ={2,7,11,15};
+        int target = 9;
+        int[] result = twoSum(numbers,target);
+        System.out.println(Arrays.toString(result));
+    }
+}
