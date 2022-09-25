704ed45d641b6ad46be1f9b81bb34261afee3d7c
debnath <dsdebnath4663@gmail.com>
2022-09-25 20:05:47 +0530

    123. Best Time to Buy and Sell Stock III https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/

--- src/main/java/com/leetcode/easy/array/BestTimetoBuyandSellStockIII.java
+++ src/main/java/com/leetcode/easy/array/BestTimetoBuyandSellStockIII.java
@@ -4,12 +4,9 @@ public class BestTimetoBuyandSellStockIII {
     public static int maxProfit(int[] prices) {
         int hold1 = Integer.MIN_VALUE, hold2 = Integer.MIN_VALUE;
         int release1 = 0, release2 = 0;
-        for (int i : prices) {
-            // Assume we only have 0 money at first
-            release2 = Math.max(release2, hold2 + i);
-            // The maximum if we've just sold 2nd stock so far.
-            hold2 = Math.max(hold2, release1 - i);
-            // The maximum if we've just buy  2nd stock so far.
+        for (int i : prices) {// Assume we only have 0 money at first
+            release2 = Math.max(release2, hold2 + i);// The maximum if we've just sold 2nd stock so far.
+            hold2 = Math.max(hold2, release1 - i);// The maximum if we've just buy  2nd stock so far.
             release1 = Math.max(release1, hold1 + i);     // The maximum if we've just sold 1nd stock so far.
             hold1 = Math.max(hold1, -i);          // The maximum if we've just buy  1st stock so far.
         }
@@ -17,7 +14,8 @@ public class BestTimetoBuyandSellStockIII {
     }
 
     public static void main(String[] arges) {
-        int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+        //int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+        int[] prices = {1,2,3,4,5};
         int result = maxProfit(prices);
         System.out.println("Maximum profit  is :" + result);
     }
