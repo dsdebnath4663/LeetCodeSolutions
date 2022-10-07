21aab32a65ce05002e69cd7943b93f2ae6359ca7
debnath <dsdebnath4663@gmail.com>
2022-10-07 20:51:12 +0530

    268. Missing Number https://leetcode.com/problems/missing-number/

---
+++ src/main/java/com/leetcode/easy/array/MissingNumber.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy.array;
+
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+    public static void main(String[] args){
+       // int[] nums ={3,0,1};
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+       int result =missingNumber(nums);
+       System.out.println("missingNumber is:  "+result);
+    }
+}
