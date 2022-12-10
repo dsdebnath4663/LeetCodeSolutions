989790fb296bd1478950d950f81b0d591b00f2c4
debnath <dsdebnath4663@gmail.com>
2022-12-10 19:25:17 +0530

    198. House Robber https://leetcode.com/problems/house-robber/

---
+++ src/main/java/com/leetcode/medium/dp/HouseRobber.java
@@ -0,0 +1,20 @@
+package com.leetcode.medium.dp;
+
+public class HouseRobber {
+    public static int rob(int[] nums) {
+        int n=nums.length;
+        int x=nums[0];
+        int y=0;
+        for(int i=1; i<n; i++){
+            int nx=y+nums[i];
+            int ny=Math.max(x, y);
+            x=nx;
+            y=ny;
+        }
+        return Math.max(x, y);
+    }
+public static void main (String[] args){
+        int [] nums = {1,2,3,1};
+        System.out.println(rob(nums));
+}
+}
