bb88bf61e1686a3a3613e514759e6159b13c0113
debnath <dsdebnath4663@gmail.com>
2023-01-07 22:02:59 +0530

    PacificAtlantic https://leetcode.com/problems/pacific-atlantic-water-flow/

---
+++ src/test/java/com/leetcode/medium/graph/dfs/PacificAtlanticWaterFlowDFSTest.java
@@ -0,0 +1,38 @@
+package com.leetcode.medium.graph.dfs;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.List;
+
+public class PacificAtlanticWaterFlowDFSTest  {
+
+
+    PacificAtlanticWaterFlowDFS obj = new PacificAtlanticWaterFlowDFS();
+
+    @Test
+    public void pacificAtlanticTest() {
+        int[][] heights = {
+                {1, 2, 2, 3, 5},
+                {3, 2, 3, 4, 4},
+                {2, 4, 5, 3, 1},
+                {6, 7, 1, 4, 5},
+                {5, 1, 1, 2, 4}};
+        List<List<Integer>> expected =
+                obj.pacificAtlantic(heights);
+        System.out.print(expected);
+    }
+
+    @Test
+    public void pacificAtlanticTest3() {
+        int[][] heights = {
+                {1, 2, 2, 3, 5, 1, 1, 1, 3},
+                {3, 2, 3, 4, 4, 2, 2, 2, 3},
+                {2, 4, 5, 3, 2, 1, 5, 1, 4},
+                {6, 7, 1, 4, 5, 1, 6, 4, 2},
+                {5, 1, 1, 2, 4, 4, 1, 1, 4}};
+        List<List<Integer>> expected =
+                obj.pacificAtlantic(heights);
+        System.out.print(expected);
+    }
+}
