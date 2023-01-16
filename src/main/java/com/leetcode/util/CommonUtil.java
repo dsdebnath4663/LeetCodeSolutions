3a2994f257851993344d153d1d8dff0b751faf19
debnath <dsdebnath4663@gmail.com>
2023-01-16 09:53:50 +0530

    CommonUtil

---
+++ src/main/java/com/leetcode/util/CommonUtil.java
@@ -0,0 +1,19 @@
+package com.leetcode.util;
+
+import java.util.Arrays;
+
+public class CommonUtil {
+    public static void print2D(int mat[][]) {
+        for (int[] row : mat)
+            System.out.println(Arrays.toString(row));
+    }
+
+    public static void print2D(boolean mat[][]) {
+        for (boolean[] row : mat)
+            System.out.println(Arrays.toString(row));
+    }
+    public static void print2D(char[][] mat) {
+        for (char[] row : mat)
+            System.out.println(Arrays.toString(row));
+    }
+}
