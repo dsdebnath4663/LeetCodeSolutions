699b7d361f18e2263e2d9dd6609516ba802969d1
debnath <dsdebnath4663@gmail.com>
2023-03-11 13:00:45 +0530

    238. Product of Array Except Self https://leetcode.com/problems/product-of-array-except-self/

---
+++ src/test/java/com/leetcode/medium/array/ProductofArrayExceptSelfTest.java
@@ -0,0 +1,29 @@
+package com.leetcode.medium.array;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductofArrayExceptSelfTest  {
+
+    ProductofArrayExceptSelf object =new ProductofArrayExceptSelf();
+
+    @Test
+    public void DifferentWaystoAddParentheses() {
+      int[] nums = {1,2,3,4};
+      int[] expected = {24,12,8,6};
+      int[] actual = object.productExceptSelf(nums);
+      Assert.assertEquals(Arrays.toString(expected),Arrays.toString(actual));
+    }
+    @Test
+    public void DifferentWaystoAddParentheses2(){
+        int[] nums = {-1,1,0,-3,3};
+        int[] expected = {0,0,9,0,0};
+        int[] actual = object.productExceptSelf(nums);
+        Assert.assertEquals(Arrays.toString(expected),Arrays.toString(actual));
+    }
+}
