75a6e18dd8d256d086c8a375767983a098474f11
debnath <dsdebnath4663@gmail.com>
2023-06-16 08:57:58 +0530

    ContainsDuplicate https://leetcode.com/problems/contains-duplicate-ii/

--- src/main/java/com/leetcode/easy/array/ContainsDuplicate.java
+++ src/main/java/com/leetcode/easy/array/ContainsDuplicate.java
@@ -1,21 +1,24 @@
 package com.leetcode.easy.array;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class ContainsDuplicate {
     public static boolean containsDuplicate(int[] nums) {
-        if(nums==null || nums.length==0)
+        if (nums == null || nums.length == 0)
             return false;
         HashSet<Integer> hset = new HashSet<Integer>();
-        for(int idx: nums){
-            if(!hset.add(idx)){
+        for (int idx : nums) {
+            if (!hset.add(idx)) {
                 return true;
             }
         }
         return false;
     }
-    public static void main(String[] args){
-        int[]nums = {1,2,3,1};
-        System.out.println(containsDuplicate(nums));
+
+        public static void main(String[] args){
+       int[]nums = {1,2,3,1};
+       System.out.println(containsDuplicate(nums));
+
     }
 }
