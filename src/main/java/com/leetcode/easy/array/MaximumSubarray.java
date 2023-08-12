4f26d0a6cc26e23f51cdb322cf84f8804fe2d0fc
debnath <dsdebnath4663@gmail.com>
2023-08-12 10:09:37 +0530

    53. Maximum Subarray https://leetcode.com/problems/maximum-subarray/

--- src/main/java/com/leetcode/easy/array/MaximumSubarray.java
+++ src/main/java/com/leetcode/easy/array/MaximumSubarray.java
@@ -1,23 +1,22 @@
 package com.leetcode.easy.array;
 
 
-
 public class MaximumSubarray {
-        public static int maxSubArray(int[] A) {
-            int n = A.length;
-            int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
-            dp[0] = A[0];
-            int max = dp[0];
-            for(int i = 1; i < n; i++){
-                dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-                max = Math.max(max, dp[i]);
-            }
-            return max ;
+    public static int maxSubArray(int[] A) {
+        int n = A.length;
+        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = A[0];
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
         }
+        return max;
+    }
 
-public static void main (String[]arages){
-        int[]A = {-2,1,-3,4,-1,2,1,-5,4};
+    public static void main(String[] arages) {
+        int[] A = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(maxSubArray(A));
 
-}
+    }
 }
