a29a19084e3533a7a8d02ce8a09fbc8c00332137
debnath <dsdebnath4663@gmail.com>
2023-08-14 11:53:55 +0530

    GenerateSubarray

---
+++ src/main/java/com/leetcode/medium/GenerateSubarray.java
@@ -0,0 +1,30 @@
+package com.leetcode.medium;
+
+import java.math.BigInteger;
+
+public class GenerateSubarray {
+
+    static int[] arr = new int[]{3, 4, 7, 2, -3, 1, 4, 2};
+
+    static void printSubsequences(int n) {
+        /* Number of subsequences is (2**n -1)*/
+        int opsize = (int) Math.pow(2, n);
+
+        /* Run from counter 000..1 to 111..1*/
+        for (int counter = 1; counter < opsize; counter++) {
+            for (int j = 0; j < n; j++) {
+/* Check if jth bit in the counter is set
+If set then print jth element from arr[] */
+
+                if (BigInteger.valueOf(counter).testBit(j)) System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Driver method to test the above function
+    public static void main(String[] args) {
+        System.out.println("All Non-empty Subsequences");
+        printSubsequences(arr.length);
+    }
+}
