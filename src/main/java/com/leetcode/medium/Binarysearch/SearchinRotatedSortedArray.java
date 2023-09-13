5a828ff4269055f1677314a9396693fd7fff0d61
debnath <dsdebnath4663@gmail.com>
2023-09-13 18:47:45 +0530

    33. Search in Rotated Sorted Array https://leetcode.com/problems/search-in-rotated-sorted-array/

---
+++ src/main/java/com/leetcode/medium/Binarysearch/SearchinRotatedSortedArray.java
@@ -0,0 +1,32 @@
+package com.leetcode.medium.Binarysearch;
+
+public class SearchinRotatedSortedArray {
+    public static int search(int[] A, int target) {
+        int lo = 0;
+        int hi = A.length - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (A[mid] == target) return mid;
+
+            if (A[lo] <= A[mid]) {
+                if (target >= A[lo] && target < A[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > A[mid] && target <= A[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return A[lo] == target ? lo : -1;
+    }
+    public static void main(String[] args){
+        int[] A ={1};
+        int target = 0;
+        System.out.println(search (A,target));
+    }
+}
