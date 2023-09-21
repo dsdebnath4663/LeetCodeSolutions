d7ce589935e9e0a1df1d090066e8a86b901a8b75
debnath <dsdebnath4663@gmail.com>
2023-09-21 09:33:51 +0530

    791. Custom Sort String https://leetcode.com/problems/custom-sort-string/

---
+++ src/main/java/com/leetcode/medium/string/CustomSortString1.java
@@ -0,0 +1,18 @@
+package com.leetcode.medium.string;
+public class CustomSortString1 {
+    public static String customSortString(String order, String s) {
+        int[] count = new int [26];
+        for (char c : s.toCharArray()) { ++count[c - 'a']; }
+        StringBuilder sb = new StringBuilder();
+        for (char c : order.toCharArray()) {
+            while (count[c - 'a']-- > 0) { sb.append(c); }
+        }
+        for (char c = 'a'; c <= 'z'; ++c) {
+            while (count[c - 'a']-- > 0) { sb.append(c); }
+        }
+        return sb.toString();
+    }
+    public static void main (String[] args){
+        System.out.println(customSortString("cbafg","abcd"));
+    }
+}
