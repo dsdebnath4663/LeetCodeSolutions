e826a40bd4adfdf590af293deed476d75719f0b4
debnath <dsdebnath4663@gmail.com>
2023-09-26 08:44:29 +0530

    56. Merge Intervals https://leetcode.com/problems/merge-intervals/

---
+++ src/main/java/com/leetcode/medium/Intervals/MergeIntervals.java
@@ -0,0 +1,38 @@
+package com.leetcode.medium.Intervals;
+
+import java.util.LinkedList;
+
+public class MergeIntervals {
+    public static int[][] merge(int[][] intervals) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < intervals.length; i++) {
+            min = Math.min(min, intervals[i][0]);
+            max = Math.max(max, intervals[i][0]);
+        }
+        int[] range = new int[max - min + 1];
+        for (int i = 0; i < intervals.length; i++) {
+            range[intervals[i][0] - min] = Math.max(intervals[i][1] - min, range[intervals[i][0] - min]);
+        }
+        int start = 0, end = 0;
+        LinkedList<int[]> result = new LinkedList<>();
+        for (int i = 0; i < range.length; i++) {
+            if (range[i] == 0) {
+                continue;
+            }
+            if (i <= end) {
+                end = Math.max(range[i], end);
+            } else {
+                result.add(new int[] {start + min, end + min});
+                start = i;
+                end = range[i];
+            }
+        }
+        result.add(new int[] {start + min, end + min});
+        return result.toArray(new int[result.size()][]);
+    }
+    public static void main(String[] args){
+        int[][] intervals ={{1,3},{2,6},{8,10},{15,18}};
+        System.out.println(merge(intervals) );
+    }
+}
